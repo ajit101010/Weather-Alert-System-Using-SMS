b81b769c4a5438f0b6a400414bb7445e3aedaa94
captaincode01 <56216877+captaincode01@users.noreply.github.com>
2022-10-05 15:18:21 +0530

    Create celi.java

---
+++ celi.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+
+    // Math.ceil() method
+    // value greater than 5 after decimal
+    double a = 1.878;
+    System.out.println(Math.ceil(a));  // 2.0
+
+
+    // value equals to 5 after decimal
+    double b = 1.5;
+    System.out.println(Math.ceil(b));  // 2.0
+
+
+    // value less than 5 after decimal
+    double c = 1.34;
+    System.out.println(Math.ceil(c));  // 2.0
+
+  }
+}
